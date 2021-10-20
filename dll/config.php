0a9cd1852594851e50bc9fbb8924a7322078e144
sciscar <90604890+sciscar@users.noreply.github.com>
2021-10-20 13:29:16 +0200

    Update config.php

--- dll/config.php
+++ dll/config.php
@@ -1,11 +1,11 @@
 <?php
 	/*Datos de la aplicacion*/
 	$site_name="Unidad Médica UTPL";
-	$url_site="";
+	$url_site="";//Aci posem el nostre domini
 
 	/*datos de la base de datos*/
 	$db_usr="root";
 	$db_pass="-----";
-	$db_name="tutoria1db";
+	$db_name="pacientes";
 	$db_host="127.0.0.1";
 ?>
