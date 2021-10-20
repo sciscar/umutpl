8d30b8c770a0399262c48f4b9e57e0d86fad4d98
sciscar <90604890+sciscar@users.noreply.github.com>
2021-10-20 13:23:07 +0200

    Update mysql.php

--- dll/mysql.php
+++ dll/mysql.php
@@ -1,7 +1,4 @@
 <?php
 //include("config.php");
-$link = mysql_connect($db_host,$db_usr,$db_pass)
-    or die('No se pudo conectar: ' . mysql_error());
-//echo 'Connected successfully';
-mysql_select_db($db_name) or die('No se pudo seleccionar la base de datos');
+$link = new mysqli($db_host,$db_usr,$db_pass, $db_name);
 ?>
